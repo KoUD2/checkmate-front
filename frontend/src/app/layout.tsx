760065831d8656ad9d5705b16df04a7d81fccdd3
Konstantin Udod <kostik-udod@mail.ru>
2026-04-06 17:08:17 +0300

    feat: add Top.Mail.Ru counter

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -93,6 +93,17 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {/* Top.Mail.Ru counter */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var _tmr = window._tmr || (window._tmr = []); _tmr.push({id: "3755767", type: "pageView", start: (new Date()).getTime()}); (function (d, w, id) { if (d.getElementById(id)) return; var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id; ts.src = "https://top-fwz1.mail.ru/js/code.js"; var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);}; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "tmr-code");`,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://top-fwz1.mail.ru/counter?id=3755767;js=na" style={{position:"absolute",left:"-9999px"}} alt="Top.Mail.Ru" />
+          </div>
+        </noscript>
         <AuthProvider>
           <TaskCheckProvider>
             <AppLayout>{children}</AppLayout>
