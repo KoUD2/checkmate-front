799c4a4b8996877dfa01ff640ba202e26ae87183
Konstantin Udod <kostik-udod@mail.ru>
2026-05-14 11:26:34 +0300

    feat: add Yandex Metrika counter to app layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -108,6 +108,19 @@ export default function RootLayout({
             <img src="https://top-fwz1.mail.ru/counter?id=3755767;js=na" style={{position:"absolute",left:"-9999px"}} alt="Top.Mail.Ru" />
           </div>
         </noscript>
+        {/* Yandex.Metrika counter */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");ym(103365255,"init",{clickmap:true,trackLinks:true,accurateTrackBounce:true,referrer:document.referrer,url:location.href});`,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/103365255" style={{position:"absolute",left:"-9999px"}} alt="" />
+          </div>
+        </noscript>
         <PostHogProvider>
           <AuthProvider>
             <TaskCheckProvider>
